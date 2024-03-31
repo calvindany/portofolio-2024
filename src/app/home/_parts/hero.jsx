560f20aa7f46165684b131eb909fa9267d94b142
calvin <calvindany1102@gmail.com>
2024-03-31 09:11:57 +0700

    added: linkedin and github navigation
    Additional:
    - added poppins fonts to hero section

--- src/app/home/_parts/hero.jsx
+++ src/app/home/_parts/hero.jsx
@@ -1,22 +1,45 @@
 "use client";
 
-import { Box, Typography } from "@mui/material";
-import { ChakraPetch } from "@/components/Fonts";
+import { Box, Typography, Link } from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHub from "@mui/icons-material/GitHub";
+
+import { ChakraPetch, Poppins } from "@/components/Fonts";
 
 const Hero = () => {
   return (
     <>
-      <Box className="flex flex-col justify-center h-[100vh]">
+      <Box className="flex flex-col gap-[10px] justify-center h-[100vh]">
         <Typography
           variant="h1"
           className={`${ChakraPetch.ChakraPetchBold.className} text-large text-default`}
         >
           Hi! I'm, <span className="text-highlight">Calvin Danyalson</span>
         </Typography>
-        <Typography variant="p" className="text-medium text-default">
+
+        <Typography
+          variant="p"
+          className={`${Poppins.PoppinsMedium.className} text-medium text-default`}
+        >
           A <span className="text-highlight">Web Developer</span> and willing to
           learn something new when if it is comes to software engineer
         </Typography>
+
+        <Box className="flex gap-8">
+          <Link
+            href="https://www.linkedin.com/in/calvin-danyalson-a87295234/"
+            className={`${Poppins.PoppinsMedium.className} text-default flex items-center gap-2`}
+          >
+            <LinkedInIcon className="text-default" /> Calvin Danyalson
+          </Link>
+
+          <Link
+            href="https://github.com/calvindany"
+            className={`${Poppins.PoppinsMedium.className} text-default flex items-center gap-2`}
+          >
+            <GitHub className="text-default" /> calvindany
+          </Link>
+        </Box>
       </Box>
     </>
   );
--- src/components/Fonts/index.js
+++ src/components/Fonts/index.js
@@ -1,1 +1,2 @@
 export { default as ChakraPetch } from "@/components/Fonts/ChakraPetch";
+export { default as Poppins } from "@/components/Fonts/Poppins";
