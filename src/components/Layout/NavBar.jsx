f8985185b83b1ccbf95f67dc125eb1ba549110ef
calvin <calvindany1102@gmail.com>
2024-03-31 08:54:14 +0700

    changed: fixed position for  navbar

--- src/components/Layout/NavBar.jsx
+++ src/components/Layout/NavBar.jsx
@@ -1,17 +1,19 @@
-import { Box } from "@mui/material";
+import { Container, Box } from "@mui/material";
 import { NavButton } from "../Features";
 
 function NavBar() {
   const Menu = ["Home", "Tech Stack", "Projects", "Contact"];
   return (
     <>
-      <Box className="bg-transparent w-100 py-4 flex justify-end gap-12">
-        {Menu.map((menu, index) => (
-          <NavButton key={index} idx={index}>
-            {menu}
-          </NavButton>
-        ))}
-      </Box>
+      <Container maxWidth="xl" className="fixed">
+        <Box className="bg-transparent w-100 py-4 flex justify-end gap-12">
+          {Menu.map((menu, index) => (
+            <NavButton key={index} idx={index}>
+              {menu}
+            </NavButton>
+          ))}
+        </Box>
+      </Container>
     </>
   );
 }
