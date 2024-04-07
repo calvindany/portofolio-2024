5950f914eb89bbba2052857eaab6c8ec76887671
calvin <calvindany1102@gmail.com>
2024-04-07 15:32:41 +0700

    added: new data for batique project

--- src/datas/ProjectsDetail.js
+++ src/datas/ProjectsDetail.js
@@ -1,4 +1,99 @@
+/*
+  This is template for datastore project
+  {
+    title: "",
+    description: "",
+    website: "",
+    github: "",
+    desc1: "",
+    desc2: "",
+    techstack: ["", "", "", "", "", "", ""],
+    thumbnail: {
+      path: "",
+      alt: "",
+      width: "",
+      height: "",
+    },
+    galery: [
+      {
+        path: "",
+        alt: "",
+        width: "",
+        height: "",
+      },
+      {
+        path: "",
+        alt: "",
+        width: "",
+        height: "",
+      },
+      {
+        path: "",
+        alt: "  ",
+        width: "",
+        height: "",
+      },
+    ],
+  },
+*/
+
 const ProjectsDetails = [
+  {
+    title: "Batique",
+    description:
+      "This application provides posts about batik artworks in Indonesia. Users or the painters themselves can post their artwork images",
+    website: "https://batique-vcp.web.app/",
+    github: "https://github.com/VCP-Batique2023/batique-fe",
+    desc1:
+      "This application provides posts about batik artworks in Indonesia. Users or the painters themselves can post their artwork images",
+    desc2:
+      "This application was developed by a team of 3 people, and I was responsible for building the UI pages for the gallery and profile, as well as the functionality to add images.",
+    techstack: ["React JS", "Javascript", "Firebase"],
+    thumbnail: {
+      path: "/batique1.png",
+      alt: "Batique Home Page",
+      width: "550px",
+      height: "309.22px",
+    },
+    galery: [
+      {
+        path: "/batique1.png",
+        alt: "Home Page",
+        width: "550px",
+        height: "309.22px",
+      },
+      {
+        path: "/batique2.png",
+        alt: "About Page",
+        width: "550px",
+        height: "309.22px",
+      },
+      {
+        path: "/batique3.png",
+        alt: "Article Page",
+        width: "550px",
+        height: "309.22px",
+      },
+      {
+        path: "/batique4.png",
+        alt: "Sign Up Page",
+        width: "550px",
+        height: "309.22px",
+      },
+      {
+        path: "/batique5.png",
+        alt: "Galery Page",
+        width: "550px",
+        height: "309.22px",
+      },
+      {
+        path: "/batique6.png",
+        alt: "Profile page",
+        width: "550px",
+        height: "309.22px",
+      },
+    ],
+  },
   {
     title: "Rozaline",
     description:
