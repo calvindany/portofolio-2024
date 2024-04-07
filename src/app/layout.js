430d01727cd943cad49e78014073b3a8e0ae6d7f
calvin <calvindany1102@gmail.com>
2024-04-08 00:40:21 +0700

    added: tabs icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/tabs.ico" sizes="any" />
       <body>
         <Container maxWidth="xl">
           <NavBar />
