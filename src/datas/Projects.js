871056ff4ba8b41e2adb451b8b6c4f4d1892e95b
calvin <calvindany1102@gmail.com>
2024-04-07 20:32:18 +0700

    fixed: navigation to project details

--- src/datas/Projects.js
+++ src/datas/Projects.js
@@ -1,21 +1,21 @@
 const Projects = [
   {
     path: "/batique.png",
-    navigation: "",
+    navigation: "/project/1",
     alt: "Batique Website",
     width: "550px",
     height: "309.66px",
   },
   {
     path: "/rozaline.png",
-    navigation: "",
+    navigation: "/project/2",
     alt: "Rozaline Website",
     width: "550px",
     height: "309.38px",
   },
   {
     path: "/indomaju.png",
-    navigation: "",
+    navigation: "/project/3",
     alt: "Indomaju Website",
     width: "550px",
     height: "309.38px",
