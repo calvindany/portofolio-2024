f5764da53010085e88f9ed6765c6daa7bc9f8cbb
Calvin Danyalson <119773802+calvindany@users.noreply.github.com>
2024-09-06 21:52:22 +0700

    fixed: navbar position

--- src/components/Layout/NavBar.jsx
+++ src/components/Layout/NavBar.jsx
@@ -6,21 +6,26 @@ import { Navigation } from "@/datas";
 const NavBar = () => {
   return (
     <>
-      <Container
-        maxWidth="xl"
-        className="sticky top-0 hidden md:block"
-        sx={{
-          background: "linear-gradient(to right, #001f3f, #003366)",
-        }}
-      >
-        <Box className="w-100 py-8 flex justify-end gap-12">
-          {Navigation.map((menu, index) => (
-            <NavButton key={index} idx={index} url={menu.url}>
-              {menu.name}
-            </NavButton>
-          ))}
-        </Box>
-      </Container>
+        <Container
+          maxWidth="xl"
+          className="sticky top-0 hidden md:block w-fit"
+          sx={{
+            background: "#003366",
+            borderLeft: "2px solid #FFA500",
+            borderRight: "2px solid #FFA500",
+            borderBottom: "2px solid #FFA500",
+            borderBottomRightRadius: "10px",
+            borderBottomLeftRadius: "10px",
+          }}
+        >
+          <Box className="py-3 flex justify-end gap-12">
+            {Navigation.map((menu, index) => (
+              <NavButton key={index} idx={index} url={menu.url}>
+                {menu.name}
+              </NavButton>
+            ))}
+          </Box>
+        </Container>
     </>
   );
 }
